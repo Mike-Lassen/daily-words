ad5ec03c52fe6d23bb93e4ad320d15d584c1d42d
Michael Lassen <michaellassen@Michaels-MacBook-Air.local>
2026-02-01 18:51:26 +0100

    Loads genki deck on load

--- src/main/java/io/grann/words/DailyWordsApplication.java
+++ src/main/java/io/grann/words/DailyWordsApplication.java
@@ -1,13 +1,22 @@
 package io.grann.words;
 
+import io.grann.words.bootstrap.DeckCsvImporter;
+import io.grann.words.domain.Deck;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DailyWordsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DailyWordsApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(DailyWordsApplication.class, args);
+		try {
+			DeckCsvImporter deckCsvImporter = context.getBean(DeckCsvImporter.class);
+			Deck deck = deckCsvImporter.importFromClasspath("genki-deck.csv");
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
